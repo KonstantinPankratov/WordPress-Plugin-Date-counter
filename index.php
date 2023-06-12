8de6d23e5466ba5def5740546ae545bd0222dae7
Konstantin Pankratov <pankratovkm@gmail.com>
2023-06-12 11:19:16 +0200

    v2.0.3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 Plugin Name: Date counter
 Plugin URI: https://github.com/KonstantinPankratov/WordPress-Plugin-Date-counter
 Description: Date counter is a WordPress plugin that allows you to add a shortcode to count difference between two dates using start and end dates or display the current date easily.
-Version: 2.0.2
+Version: 2.0.3
 Author: Konstantin Pankratov
 Author URI: http://kopa.pw/
 */
